60abbe5d47a65ea1acf5e4af54b853691b6e7a72
Jens Schuppe <schuppe@systopia.de>
2024-10-29 13:36:43 +0100

    Adjust to support accessible accordions as of CiviCRM 5.69 (dropping support for older versions)

--- js/ActivityCreation.js
+++ js/ActivityCreation.js
@@ -14,43 +14,42 @@
 +--------------------------------------------------------*/
 
 (function ($) {
-    let contactOriginWrapper = $('#contactOriginWrapper');
-    let accordionWrappers = $('.crm-accordion-wrapper').not(contactOriginWrapper);
+  let contactOriginWrapper = $('#contactOriginWrapper');
+  let accordionWrappers = $('details.crm-accordion-bold').not(contactOriginWrapper);
 
-    // make sure there's something there...
-    let firstAccordion = accordionWrappers[0];
-    if (!firstAccordion)
-    {
-        return;
+  // make sure there's something there...
+  if (0 === accordionWrappers) {
+    return;
+  }
+
+  // collapse other accordions, if subject is not filled yet
+  if ($('#contactorigin_subject').val()) {
+    contactOriginWrapper.addClass('collapsed');
+  }
+  else {
+    accordionWrappers.addClass('collapsed').removeAttr('open');
+  }
+
+  // insert ours at the top
+  contactOriginWrapper.insertBefore(accordionWrappers.first());
+
+  // copy campaign title into subject if selected
+  $('#contactorigin_campaign').change(function () {
+    let selected_campaign = $('#contactorigin_campaign').val();
+    if (parseInt(selected_campaign)) {
+      $('#contactorigin_subject')
+        .val(CRM.vars.contactsource.campaigns[selected_campaign])
+        .change();
     }
+  });
 
-    // collapse other accordions, if subject is not filled yet
-    if (cj("#contactorigin_subject").val()) {
-        contactOriginWrapper.addClass('collapsed');
-    } else {
-        accordionWrappers.addClass('collapsed');
+  // open main contact when fields are filled
+  $('#contactorigin_subject').change(function () {
+    if ($('#contactorigin_subject').val()) {
+      $('.crm-accordion-wrapper')
+        .not(contactOriginWrapper)
+        .first()
+        .removeClass('collapsed');
     }
-
-    // insert ours at the top
-    contactOriginWrapper.insertBefore(firstAccordion);
-
-    // copy campaign title into subject if selected
-    cj("#contactorigin_campaign").change(function() {
-        let selected_campaign = cj("#contactorigin_campaign").val();
-        if (parseInt(selected_campaign)) {
-            cj("#contactorigin_subject")
-                .val(CRM.vars.contactsource.campaigns[selected_campaign])
-                .change();
-        }
-    });
-
-    // open main contact when fields are filled
-    cj("#contactorigin_subject").change(function() {
-        if (cj("#contactorigin_subject").val()) {
-            cj('.crm-accordion-wrapper')
-                .not(contactOriginWrapper)
-                .first()
-                .removeClass('collapsed');
-        }
-    });
-})(cj);
+  });
+})(CRM.$ || cj);
